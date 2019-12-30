a2ca7931b4f8575c18e737e517f5351441770e9a
Jörg Hoffmann <19joho66@gmail.com>
2019-12-30 15:22:20 +0100

    Implemented Installer

---
+++ windows/Installer.py
@@ -0,0 +1,25 @@
+#! python
+
+# the Inno Setup console-mode compiler
+iscc = 'C:\Program Files (x86)\Inno Setup 5\ISCC'
+
+# where to build the frozen program folder
+target_dir = 'frozen'
+
+# import standard modules and cx_Freeze
+import imp
+import os
+import py_compile
+import shutil
+import subprocess
+import sys
+from cx_Freeze import Executable, Freezer
+
+# access the frescobaldi_app package
+sys.path.insert(0, '..')
+
+# access meta-information such as version, etc.
+from frescobaldi_app import appinfo
+
+
+subprocess.run([iscc, "/Dhomepage="+appinfo.url , "/Dversion="+appinfo.version , "/Dauthor="+appinfo.maintainer , "/Dcomments="+appinfo.description, "/Dtarget="+target_dir, "setup.iss" ])
